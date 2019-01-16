a9c72ab952678e2ed0bd84492d8bf89aac3a28bc
Wirtos <Wirtos.new@gmail.com>
2019-01-16 02:33:52 +0200

    path sep
    Fixed path separator issue on windows, merged function files into one, main py code reformatted, added reqs.txt

--- funcs.py
+++ funcs.py
@@ -1,24 +1,24 @@
-def splitter(gif, out):
-    from PIL import Image
-  # https://gist.github.com/revolunet/848913
+import os
+from PIL import Image
+from shutil import copyfile
 
-    frame = Image.open(gif)
-    n = 0
-    while frame:
-        frame.save(f'{out}/{n:04d}.png', 'PNG')
-        n += 1
-        try:
-            frame.seek(n)
-        except EOFError:
-            break
 
-    return out
+def desc(out, width, height, fps):
+    parts = []
+    with os.scandir(out) as files:
+        for i in files:
+            if i.is_dir() and i.name.startswith('part'):
+                parts.append(i.name)
+
+    with open(f'{out}{os.sep}desc.txt', 'w') as txt:
+        txt.write(f'{width} {height} {fps}\n')
+        for i in parts:
+            txt.write(f'p 0 0 {i}\n')
+
+    return True
 
 
 def mirror(path):
-    import os
-    from shutil import copyfile
-
     files = os.listdir(path)
     ext = os.path.splitext(files[0])[-1]
     for i in files:
@@ -26,18 +26,43 @@ def mirror(path):
     files.sort(reverse=True)
     n, l = int(files[0]), len(files[0])
     files = files[1:-1]
+
     for i in files:
         n += 1
-        copyfile(f'{path}/{i}{ext}', f'{path}/{n:0{l}d}{ext}')
+        copyfile(f'{path}{os.sep}{i}{ext}', f'{path}{os.sep}{n:0{l}d}{ext}')
 
 
-def desc(out, width, height, fps):
-    import os
+def splitter(gif, out):
+    # https://gist.github.com/revolunet/848913
+    frame = Image.open(gif)
+    n = 0
+    while frame:
+        frame.save(f'{out}{os.sep}{n:04d}.png', 'PNG')
+        n += 1
+        try:
+            frame.seek(n)
+        except EOFError:
+            break
+    return out
 
-    parts = [i.name for i in os.scandir(out) if i.is_dir() and i.name.startswith('part')]
-    with open(f'{out}/desc.txt', 'w') as txt:
-        txt.write(f'{width} {height} {fps}\n')
-        for i in parts:
-            txt.write(f'p 0 0 {i}\n')
 
-    return True
+def new_part(out):
+    n = 0
+    while os.path.exists(f'{out}{os.sep}part{n}'):
+        n += 1
+    p = f'{out}{os.sep}part{n}'
+    os.makedirs(p)
+    return p
+
+
+def ratio(out):
+    from PIL import Image
+    with os.scandir(out) as files:
+        for i in files:
+            if i.is_dir() and i.name.startswith('part'):
+                with os.scandir(i) as images:
+                    for j in images:
+                        if j.is_file() and j.name.endswith('.png'):
+                            img = Image.open(j.path)
+                            return img.size[0] / float(img.size[1])
+    return None
--- main.py
+++ main.py
@@ -1,48 +1,47 @@
-import os
-from sys import argv
-from optparse import OptionParser
-from tempfile import TemporaryDirectory
-from zipfile import ZipFile, ZIP_STORED
+def main():
+    import os
+    from optparse import OptionParser
+    from tempfile import TemporaryDirectory
+    from zipfile import ZipFile, ZIP_STORED
 
-from funcs import splitter, mirror, desc
-from other import new_part, ratio
+    from funcs import new_part, ratio, splitter, mirror, desc
 
-usage = 'usage: %prog [options] filename fps'
-description = 'Input a GIF file, get a bootanimation.zip of it.'
-prsr = OptionParser(usage=usage, description=description)
+    usage = 'usage: %prog [options] filename fps'
+    description = 'Input a GIF file, get a bootanimation.zip of it.'
+    prsr = OptionParser(usage=usage, description=description)
 
-prsr.add_option(
-    '-m',
-    '--mirror',
-    default = False,
-    dest    = 'mirror',
-    action  = 'store_true',
-    help    = 'mirror the gif so it will be a perfect loop'
-)
-prsr.add_option(
-    '-r',
-    '--res',
-    default = 1080,
-    dest    = 'res',
-    type    = 'int',
-    action  = 'store',
-    metavar = 'RESOLUTION',
-    help    = 'specify a resolution (only needs width, default is 1080)'
-)
+    prsr.add_option(
+        '-m',
+        '--mirror',
+        default=False,
+        dest='mirror',
+        action='store_true',
+        help='mirror the gif so it will be a perfect loop'
+    )
+    prsr.add_option(
+        '-r',
+        '--res',
+        default=1080,
+        dest='res',
+        type='int',
+        action='store',
+        metavar='RESOLUTION',
+        help='specify a resolution (only needs width, default is 1080)'
+    )
+    options, args = prsr.parse_args()
+    with TemporaryDirectory() as out:
+        gif, fps = args[0], args[1]
+        p0 = splitter(gif, new_part(out))
+        if options.mirror:
+            mirror(p0)
+        w, h = options.res, int(options.res / ratio(out))
+        desc(out, w, h, fps)
+        out = os.path.abspath(out)
+        with ZipFile('bootanimation.zip', 'w', ZIP_STORED) as zp:
+            for root, dirs, files in os.walk(out):
+                for file in files:
+                    zp.write(filename=os.path.abspath(root + os.sep + file),
+                             arcname=(root.replace(out, "") + os.sep + file))
 
-(options, args) = prsr.parse_args()
-
-with TemporaryDirectory() as out:
-    gif, fps = args[0], args[1]
-    p0 = splitter(gif, new_part(out))
-    if options.mirror:
-        mirror(p0)
-    w, h = options.res, int(options.res / ratio(out))
-    desc(out, w, h, fps)
-
-    with ZipFile('bootanimation.zip', 'w', ZIP_STORED) as zp:
-        for root, dirs, files in os.walk(out):
-            for file in files:
-                p = f'{root}/{file}'
-                arcp = '/'.join(p.split('/')[3:])
-                zp.write(p, arcp)
+if __name__ == '__main__':
+    main()
